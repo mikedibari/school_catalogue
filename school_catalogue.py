f6c596f0ddb2851bd8e654d5770dd22440337626
Mike DiBari <mike.dibari@comcast.net>
2023-06-28 10:54:41 -0400

    add PrimarySchool class

--- school_catalogue.py
+++ school_catalogue.py
@@ -17,7 +17,21 @@ class School():
         self.numberOfStudents = newNumberOfStudents
     # return the string that's displayed when you print an object of the class
     def __repr__(self):
-        return f'A {self.level} school named {self.name} with {self.numberOfStudents} students'
+        return f'A {self.level} school named {self.name} with {self.numberOfStudents} students.'
+# inherits from School
+class PrimarySchool(School):
+    def __init__(self, name, numberOfStudents, pickupPolicy):
+        super().__init__(name, "primary", numberOfStudents)
+        self.pickupPolicy = pickupPolicy
+
+    def getPickupPolicy(self):
+        return self.pickupPolicy
+
+    def __repr__(self):
+        # use super() to avoid repeating code
+        parentRepr = super().__repr__()
+        return parentRepr + f' The pickup policy is {self.pickupPolicy}.'
+    
 
 a = School("Cambridge", "high", 300)
 print(a)
@@ -25,3 +39,7 @@ print(a.getName())
 print(a.getLevel())
 a.setNumberOfStudents(400)
 print(a.getNumberOfStudents())
+
+b = PrimarySchool("Boston", 500, "'Pickup Allowed'")
+print(b.getPickupPolicy())
+print(b)
